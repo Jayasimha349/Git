50201dfe0b7da580acd6f2cc334fd7eb23208b80
jayasimha parne <jayasimhaparne@gmail.com>
2019-08-25 22:42:02 -0400

    variable declaration

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#1bin/bash
+
+echo " I am trying to learn how to declare variables in shell scripting"
+a=jaya
+
+#we can acess the declared variable by using the $sign.
+
+echo "welcome $a simha reddy"
+
