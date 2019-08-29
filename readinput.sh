176faec1e2c17fce1dcc6bfb0e25212c68673863
jayasimha parne <jayasimhaparne@gmail.com>
2019-08-29 07:59:28 -0400

    trying to add readinput.sh

---
+++ readinput.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# read command takes input from the user and stores the value in the variable
+
+read -p 'Enter your name :' name
+
+read -p 'Enter your course :' course
+
+echo" Your name is $name and course is $course
+
+
