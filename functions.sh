80126a8b4c6bc3c0bb342ebab60d498807a00c8d
Jayasimha349 <jayasimhaparne@gmail.com>
2019-08-30 08:36:59 -0400

    i am updating my remote repository

---
+++ functions.sh
@@ -0,0 +1,12 @@
+#1/bin/bash
+
+#functions
+
+#In the functions we declare the function and call the funnction
+#In the function we will not use exit command beacause it exits completely from the function instead we uses return command to come out of the function
+#If we will give return command in the middle of the function it comes out from the function immediatley after it encounters the return command
+#Variables in the function we can acess variables in the function in the main program and vice versa.
+#if we declare variable inside the function and gives the local the we can acess it only in the function.
+
+#
+
---
+++ redirectors.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#&> will redirect both output and error to the same
+#2> will redirect error to the different file
+#exit status:0 is globally suscesful
+#(1-255) are unsuscefull 
+#you are allowed to choose between (1-125) and from 126 to 255 system will use.
+
+#Quotes:
+#Single quotes
+#Double quotes
+#
+#
+#
+#
+#
+#
+#
+
+
